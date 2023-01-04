74de2be6483a2016379086fbe522163b8edb06eb
ethan-cyj <121071410+ethan-cyj@users.noreply.github.com>
2023-01-04 18:04:27 +0800

    added dropdown menu animation

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //alert("welcome")
 
-const menuButton = document.getElementById("menu");
-const menuContent = document.getElementById("menu-content");
-var expanded = menuButton.getAttribute("aria-expanded");
-menuContent.style.display = "none";
-
-menuButton.addEventListener("click", (event) => {
-    if (menuContent.style.display == "none") {
-        menuContent.style.display = "block";
-    } else {
-        menuContent.style.display = "none";
-    }
-})
+//const menuButton = document.getElementById("menu");
+//const menuContent = document.getElementById("menu-content");
+//var expanded = menuButton.getAttribute("aria-expanded");
+//menuContent.style.display = "none";
+//
+//menuButton.addEventListener("click", (event) => {
+//    if (menuContent.style.display == "none") {
+//        menuContent.style.display = "block";
+//    } else {
+//        menuContent.style.display = "none";
+//    }
+//})
 // navbar menu dropdown >>>
 //const menuButton = document.getElementById("menu");
 //const menuContent = document.getElementById("menu-content");
